8673c61f8ec33abf8d4524087c446c1f8379ddf0
Muhammad Farhan <=>
2024-07-27 01:17:47 +0700

    Add img, modified index.php

--- index.php
+++ index.php
@@ -23,6 +23,7 @@ if (isset($_POST["submit"])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="Assets/img/logo/logo.png" type="image/x-icon">
     <title>My Portfolio</title>
     <!-- MyCss -->
     <link rel="stylesheet" href="Css/style.css">
